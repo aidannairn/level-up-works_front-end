66c2c8ff87447a77e61a6d3bbce38e3b212aa3e9
aidannairn <aidan.nairn95@gmail.com>
2022-06-14 17:15:37 +1200

    Create Footer and add to App.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main> {/* Add components within main */}
+        
+      </main>
+      <Footer />
     </div>
   );
 }
