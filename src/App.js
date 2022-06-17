a9e9605716feebb7fcf66542d4f4b375ef732b5d
Louis Dott <lndenterprises@icloud.com>
2022-06-17 20:37:06 +1200

    made nav bar for sudent profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// import Header from "./components/MainHeader";
-// import Homepage from "./components/homepage/Homepage";
-// import Footer from "./components/MainFooter";
+ import Header from "./components/MainHeader";
+ import Homepage from "./components/homepage/Homepage";
+ import Footer from "./components/MainFooter";
+import TeacherNavBar from "./components/studentProfiles/teacherNavBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <TeacherNavBar/>
     </div>
   );
 }
