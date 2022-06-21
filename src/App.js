10ede86fc180ef35c6d8b4b7da8bef72308d726f
Wilem Tipene <wilemtipene@gmail.com>
2022-06-21 21:44:50 +1200

    finished up, just need to pull to put in correct header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-//import ProjectHeading from './Components/ProjectsPage/ProjectsHeading'
-//import ProjectNav from './Components/ProjectsPage/ProjectsNav'
-//import Projects from './Components/ProjectsPage/Projects'
+import ProjectHeading from './Components/ProjectsPage/ProjectsHeading'
+import ProjectNav from './Components/ProjectsPage/ProjectsNav'
+import Projects from './Components/ProjectsPage/Projects'
 //import Homepage from "./components/homepage/Homepage";
-//import Header from './Components/MainHeader'
+import Header from './Components/MainHeader'
+import Sidebar from './Components/ProjectsPage/Sidebar'
 // import Homepage from "./components/homepage/Homepage";
-//import Footer from './Components/MainFooter'
+import Footer from './Components/MainFooter'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <ProjectHeading/>
+      <ProjectNav/>
+      <Sidebar/>
+      <Projects/>
       <Footer />
     </div>
   );
--- src/components/ProjectsPage/ProjectsNav.js
+++ src/components/ProjectsPage/ProjectsNav.js
@@ -5,15 +5,32 @@ const ProjectsNav = () => {
   return (
       <div id='ProjectsNav'>
         <div id='levelButton'>
-          <button>BEGINNER</button>
-          <button>INTERMEDIATE</button>
-          <button>ADVANCED</button>
+          <label class="radio-button">
+            <input type="radio" name="level"></input>
+            <span>BEGINNER</span>
+          </label>
+          <label class="radio-button">
+            <input type="radio" name="level"></input>
+            <span>INTERMEDIATE</span>
+          </label>
+          <label class="radio-button">
+            <input type="radio" name="level"></input>
+            <span>ADVANCED</span>
+          </label>
         </div>
         <div id='viewButton'>
-        <p>SHOW</p>
-        <button>25</button>
-          <button>50</button>
-          <button>100</button>
+          <label class="radio-button2">SHOW
+            <input type="radio" name="show"></input>
+            <span>25</span>
+          </label>
+          <label class="radio-button2">
+            <input type="radio" name="show"></input>
+            <span>50</span>
+          </label>
+          <label class="radio-button2">
+            <input type="radio" name="show"></input>
+            <span>100</span>
+          </label>
         </div>
       </div>
   )
