767794b915fc95578b44b78b6f5edb6d35e217b0
aidannairn <aidan.nairn95@gmail.com>
2022-06-22 11:50:08 +1200

    Render elements and alter CSS of Sidebar dependant on sidebarExpanded state.

--- src/components/project-builder/ProjectBuilder.js
+++ src/components/project-builder/ProjectBuilder.js
@@ -6,7 +6,7 @@ import Sidebar from "./Sidebar"
 import '../../styles/project-builder/project-builder.css'
 
 const ProjectBuilder = ({ currentUser, projectItems }) => {
-  const [currentMenuItem, setCurrentMenuItem] = useState(projectItems[4])
+  const [currentMenuItem, setCurrentMenuItem] = useState(projectItems[0])
 
   return (
     <div id='project-builder-container'>
