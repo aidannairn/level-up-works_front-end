d118d14f81867a96b559d42a408ae0556da44c18
aidannairn <aidan.nairn95@gmail.com>
2022-06-26 12:10:47 +1200

    Move reusable project builder items from StudentBuilder to ProjectBuilder.

--- src/components/project-builder/ProjectBuilder.js
+++ src/components/project-builder/ProjectBuilder.js
@@ -2,12 +2,127 @@ import { useState } from "react"
 
 import ProjectBuilderContent from "./ProjectBuilderContent"
 import Sidebar from "./Sidebar"
+import LearningObjectives from './LearningObjectives'
+import Instruction from './Instruction'
+import VideoTutorial from './VideoTutorial'
 
 import '../../styles/project-builder/project-builder.css'
 
 const ProjectBuilder = ({ currentUser, projectItems }) => {
   const [currentMenuItem, setCurrentMenuItem] = useState(projectItems[0])
 
+  const learningObjectives = {
+    id: 'learningObjectives',
+    component: LearningObjectives,
+    menuItem: 'Learning Objectives',
+    icon: 'objectives.png',
+    contents: {
+      heading: 'Explore Scratch Blocks',
+      desc: 'Learn the basic function of some scratch blocks such as "say", "wait", "go to" and "hide".',
+      blocks: [
+        {
+          type: 'look',
+          mainBlock: 'look-main-block.png',
+          blockComponents: [
+            'look-component-1.png',
+            'look-component-2.png',
+          ]
+        },
+        {
+          type: 'control',
+          mainBlock: 'control-main-block.png',
+          blockComponents: [
+            'control-component-1.png'
+          ]
+        },
+        {
+          type: 'motion',
+          mainBlock: 'motion-main-block.png',
+          blockComponents: [
+            'motion-component-1.png'
+          ]
+        },
+      ]
+    }
+  }
+
+  const instructions = {
+    id: 'instructions',
+    component: Instruction,
+    isArrowNavEnabled: true,
+    menuItem: 'Instructions',
+    icon: 'steps.png',
+    contents: [
+      { 
+        id: 1,
+        elements: [
+          {
+            type: 'heading',
+            content: 'Join Scratch'
+          },
+          {
+            type: 'paragraph',
+            content: "If you haven't used Scratch before, you will need to join Scratch first."
+          },
+          {
+            type: 'paragraphWithLink',
+            content: {
+              textBefore: "Go to https://scratch.mit.edu. Click on",
+              textAfter: '',
+              href: 'https://scratch.mit.edu',
+              text: 'Join Scratch.',
+              linkColor: '#F91D85'
+            }
+          },
+          {
+            type: 'image',
+            content: 'instructions-join-scratch.png'
+          },
+          {
+            type: 'paragraph',
+            content: "Follow the instructions to join. You will need a username and a password that you will remember. If possible, you should also verify your email address so that you can Share projects later. Ask your teacher to help with this step if you don't have an email address, or if you are not sure what to do."
+          }
+        ]
+      },
+      {
+        id: 2,
+        elements: [
+          {
+            type: 'heading',
+            content: 'Page Two'
+          }
+        ]
+      }
+    ]
+  } 
+
+  const videoTutorial = { 
+    id: 'videoTutorial',
+    component: VideoTutorial,
+    isArrowNavEnabled: true,
+    menuItem: 'Video Tutorial',
+    icon: 'video.png',
+    contents: [
+      { id: 1, youtubeID: '-SjuiawRMU4'},
+      { id: 2, youtubeID: 'y5-cApGYXss'},
+      { id: 3, youtubeID: '-MIKW9Wcml0'},
+    ]
+  }
+
+  const projectBuilderItems = {
+    learningObjectives,
+    instructions,
+    videoTutorial
+  }
+
+  projectItems.map((projectItem, index) => {
+    for (const key in projectItem) {
+      if (projectItem[key] === 'insert') {
+        projectItems[index] = projectBuilderItems[key]
+      }
+    }
+  })
+  
   return (
     <div id='project-builder-container'>
       <Sidebar userImg={currentUser.image} 
--- src/pages/StudentBuilder.js
+++ src/pages/StudentBuilder.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
-import LearningObjectives from '../components/project-builder/LearningObjectives'
-import SubmitProject from '../components/project-builder/SubmitProject'
-import Instruction from '../components/project-builder/Instruction'
-import VideoTutorial from '../components/project-builder/VideoTutorial'
-import MakeProject from '../components/project-builder/MakeProject'
 import MainHeader from '../components/header/MainHeader'
+import MakeProject from '../components/project-builder/MakeProject'
+import SubmitProject from '../components/project-builder/SubmitProject'
 import ProjectBuilder from '../components/project-builder/ProjectBuilder'
 
 const StudentBuilder = () => {
@@ -33,104 +30,6 @@ const StudentBuilder = () => {
   }
 
   // START Student Project Builder Views
-  const learningObjectives = {
-    id: 'learningObjectives',
-    component: LearningObjectives,
-    menuItem: 'Learning Objectives',
-    icon: 'objectives.png',
-    contents: {
-      heading: 'Explore Scratch Blocks',
-      desc: 'Learn the basic function of some scratch blocks such as "say", "wait", "go to" and "hide".',
-      blocks: [
-        {
-          type: 'look',
-          mainBlock: 'look-main-block.png',
-          blockComponents: [
-            'look-component-1.png',
-            'look-component-2.png',
-          ]
-        },
-        {
-          type: 'control',
-          mainBlock: 'control-main-block.png',
-          blockComponents: [
-            'control-component-1.png'
-          ]
-        },
-        {
-          type: 'motion',
-          mainBlock: 'motion-main-block.png',
-          blockComponents: [
-            'motion-component-1.png'
-          ]
-        },
-      ]
-    }
-  }
-
-  const instructions = {
-    id: 'instructions',
-    component: Instruction,
-    isArrowNavEnabled: true,
-    menuItem: 'Instructions',
-    icon: 'steps.png',
-    contents: [
-      { 
-        id: 1,
-        elements: [
-          {
-            type: 'heading',
-            content: 'Join Scratch'
-          },
-          {
-            type: 'paragraph',
-            content: "If you haven't used Scratch before, you will need to join Scratch first."
-          },
-          {
-            type: 'paragraphWithLink',
-            content: {
-              textBefore: "Go to https://scratch.mit.edu. Click on",
-              textAfter: '',
-              href: 'https://scratch.mit.edu',
-              text: 'Join Scratch.',
-              linkColor: '#F91D85'
-            }
-          },
-          {
-            type: 'image',
-            content: 'instructions-join-scratch.png'
-          },
-          {
-            type: 'paragraph',
-            content: "Follow the instructions to join. You will need a username and a password that you will remember. If possible, you should also verify your email address so that you can Share projects later. Ask your teacher to help with this step if you don't have an email address, or if you are not sure what to do."
-          }
-        ]
-      },
-      {
-        id: 2,
-        elements: [
-          {
-            type: 'heading',
-            content: 'Page Two'
-          }
-        ]
-      }
-    ]
-  } 
-
-  const videoTutorial = { 
-    id: 'videoTutorial',
-    component: VideoTutorial,
-    isArrowNavEnabled: true,
-    menuItem: 'Video Tutorial',
-    icon: 'video.png',
-    contents: [
-      { id: 1, youtubeID: '-SjuiawRMU4'},
-      { id: 2, youtubeID: 'y5-cApGYXss'},
-      { id: 3, youtubeID: '-MIKW9Wcml0'},
-    ]
-  }
-
   const makeProject = { 
     id: 'makeProject',
     component: MakeProject,
@@ -138,8 +37,7 @@ const StudentBuilder = () => {
     menuItem: 'Make Project',
     icon: 'new-project.png',
     contents: [
-      { id: 1, src: 'project.png'},
-      { id: 2, src: 'project.png'},
+      { id: 1, src: 'project.png'}
     ]
   }
 
@@ -182,9 +80,9 @@ const StudentBuilder = () => {
 // END Student Project Builder Views
 
   const projectItems = [
-    learningObjectives,
-    instructions,
-    videoTutorial,
+    { learningObjectives: 'insert' },
+    { instructions: 'insert' },
+    { videoTutorial: 'insert' },
     makeProject,
     submitProject,
     bonusChallenge,
