eed88b0563400e66fd1966066886f6d5ef95571d
aidannairn <aidan.nairn95@gmail.com>
2022-07-14 21:32:50 +1200

    Handle merge conflicts.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route,
 } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import StudentBuilder from './pages/StudentBuilder'
-=======
- import Header from "./components/MainHeader";
- import Homepage from "./components/homepage/Homepage";
- import Footer from "./components/MainFooter";
-import TeacherNavBar from "./components/studentProfiles/teacherNavBar";
->>>>>>> louis
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/student-project-builder' element={<StudentBuilder />} />
         </Routes>
       </BrowserRouter>
-=======
-      <TeacherNavBar/>
->>>>>>> louis
     </div>
   );
 }
